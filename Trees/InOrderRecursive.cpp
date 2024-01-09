920562dcff11f34de3fa1e205fdbfa58762a2cd1
Sonali Rana <ranasonali987@gmail.com>
2024-01-09 18:26:44 +0530

    pre, In , Post order traversal recursive

---
+++ Trees/InOrderRecursive.cpp
@@ -0,0 +1,25 @@
+// Recursive way Inorder
+class Solution
+{
+public:
+    void solve(TreeNode *root, vector<int> &ans)
+    {
+
+        if (root == NULL)
+            return;
+
+        solve(root->left, ans);
+        ans.push_back(root->val);
+        solve(root->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+
+        solve(root, ans);
+
+        return ans;
+    }
+};
---
+++ Trees/PreOrderRecursive.cpp
@@ -0,0 +1,22 @@
+// recursive leetcode
+class Solution
+{
+public:
+    void solve(TreeNode *root, vector<int> &ans)
+    {
+        if (root == NULL)
+            return;
+
+        ans.push_back(root->val);
+        solve(root->left, ans);
+        solve(root->right, ans);
+    }
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+        solve(root, ans);
+        return ans;
+    }
+};
